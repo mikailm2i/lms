739dcb7be4d7352880bdb81247fc45acaad1fb45
mikailm2i <mikail.h.pro@gmail.com>
2026-03-13 15:15:10 +0100

    Add files via upload
    Ce fichier permet de déclarer les variables utilisées dans l’infrastructure Terraform.
    
    Il sert à rendre la configuration paramétrable et réutilisable en évitant de coder en dur certaines valeurs.
    
    Les variables principales définies sont :
    
    project_id : identifiant du projet Google Cloud
    region : région de déploiement des ressources
    zone : zone utilisée pour les ressources Compute Engine et GKE
    
    L’utilisation de variables permet d’adapter facilement l’infrastructure à différents environnements.

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "ID du projet GCP"
+  type        = string
+}
+
+variable "region" {
+  description = "Région GCP"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "zone" {
+  description = "Zone GCP"
+  type        = string
+  default     = "europe-west1-b"
+}
